f8d38fca6f223fe158e7a1b20dc777c898007ba5
abhishek272102 <avjatt5t@gmail.com>
2025-12-06 14:06:48 +0530

    cors origin update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 export default  async function handler(req, res) {
     // ---- CORS ----
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://project-live-crypto-dashboard-web-app.webflow.io");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
